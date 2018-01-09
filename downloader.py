e9a0eae5392236193193d9f281f8181d126693c5
Urban Knupleš <urban@knuples.net>
2018-01-09 07:12:19 +0100

    Rewrote downloaded.py to use Selenium and a headless Firefox for interacting with browser.

--- downloader.py
+++ downloader.py
@@ -10,25 +10,62 @@
     Downloads the data of the URL:  https://feri.um.si/urniki5/groups.php.
 
 """
-import requests
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.firefox.options import Options
+
 import time
+import os
+import sys, getopt
 
-params = {
-	"date_field": time.strftime("%d.%m.%Y"),
-	"iCal_data":"group_week",
-	"pagename":"groups",
-	"year_index":"1",
-	"year_response":"3",
-	"branch_id":"58",
-	"branch_index":"1"
-}
-
-headers = {"User-Agent":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0"}
+filename = "data.ics"
+url = "http://wise-tt.com/wtt_um_feri/"
+course = 'RIT UN'
 
 def download():
-    s = requests.Session()
-    s.headers.update(headers)
+    profile = webdriver.FirefoxProfile()
 
-    r = s.post("https://feri.um.si/urniki5/lib/iCal.php?r=group_ical&type=group_week", data=params)
+    profile.set_preference("browser.download.folderList", 2)
+    profile.set_preference("browser.download.manager.showWhenStarting", False)
+    profile.set_preference("browser.download.dir", os.getcwd())
+    profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/octet-stream, text/calendar,application/vnd.sus-calendar,text/x-vcalendar")
 
-    return(r.content)
+    options = Options()
+    options.add_argument("--headless")
+
+    browser = webdriver.Firefox(firefox_profile=profile, firefox_options=options)
+    
+    browser.get("http://wise-tt.com/wtt_um_feri/")
+    wait = WebDriverWait(browser, 4)
+    
+    print("Starting click for course.")
+    element = wait.until(EC.presence_of_element_located((By.ID, "form:j_idt85_label")))
+    element.click()
+    print("Clicked course menu.")
+
+
+    print("\nStarting click for {}.".format(course))
+    elementType = wait.until(EC.presence_of_element_located((By.ID, "form:j_idt85_6")))
+    elementType.click()
+    print("Clicked {}.".format(course))
+
+    time.sleep(5)
+    print("\nStarting to download file")
+    elementType = wait.until(EC.presence_of_element_located((By.ID, "form:j_idt151")))
+    elementType.click()
+    print("\nDownloaded file.")
+
+    time.sleep(2)
+    os.rename("calendar.ics", filename)
+
+    return filename
+
+if __name__ == "__main__":
+    if( len(sys.argv) - 1 == 1 ):
+        pass
+
+    if download():
+        print("Successfuly finished downloading.")
