80b6df1467b8e4f5288be621201096001fc8977e
Urban Knupleš <urban@knuples.net>
2018-01-09 15:07:02 +0100

    Made a class Extractor

--- extractor.py
+++ extractor.py
@@ -9,61 +9,77 @@
 """
     Extracts the data from the downloaded file
 """
-import downloader as dl
-from course import *
+from lib.course import *
 import sys
 
-# Removes unwanted characters
-def removeUnwantedCharacters(list):
-    tmp_test = []
-    for data in list:
-        # Gives back everything right of the ":" character
-        data = data.split(":")[1]
-        
-        # Removes whitespace left and right of string
-        data = data.rstrip().lstrip()
-        tmp_test.append(data)
-    
-    return tmp_test
-
-# Removes data specified params
-def removeListData(list, params):
-    for index, data in enumerate(list):
-        for unwantedData in params:
-            if unwantedData in data:
-                del list[index]
-
-    list = removeUnwantedCharacters(list)
-
-    # Location doesn't want to be found, so I bruteforce remove it
-    for i in params:
-        if "LOC" in i:
-            return list[1:]
-    
-    return list
-
-
 # returns list of every occuring class in week
-def getCourseData(file):   
-    classList = [] # List of 
+class Extractor:
+    __classList = None
+    __unwantedLines = None
 
-    for count, line in enumerate(file):
-        # initialize variables and list 
-        if "BEGIN:VEVENT" in line:
-            course = []
+    def __init__(self, unwanted):
+        self.__unwantedLines = unwanted
+        self.__classList = []
 
-        elif "END:VEVENT" in line:
-            # Removes the lines that shouldn't be in list
-            tmp = removeListData(course, {"UID","DTSTAMP","LOCATION"}) 
-            data = Course()
-            data.setValues(tmp)
-            classList.append( data )
+    # Removes entries in list based on what the unwanted lines
+    # the user suggested
+    def __rmEntries(self, list):
+        for index, data in enumerate(list):
+            for unwanted in self.__unwantedLines:
+                if unwanted in data:
+                    del list[index]
 
-        else:
-            try:
-                # Fuck newlines
-                course.append(line.strip("\n"))
-            except:
+        list = self.__rmUnwantedChar(list)
+
+        # Location doesn't want to be found, so I bruteforce removed it
+        for i in self.__unwantedLines:
+            if "LOC" in i:
+                return list[1:]
+        
+        return list
+
+    # Removes left of ":" and whitespaces between entire text
+    def __rmUnwantedChar(self, list):
+        tmpList = []
+        
+        for entiry in list:
+            entiry = entiry.split(":")[1].strip()
+            tmpList.append(entiry)
+
+        return tmpList
+
+    def getClassList(self):
+        return self.__classList
+    
+
+    def extractFromFile(self, file):
+        for index, line in enumerate(file):
+            # initialize variables and list 
+            if "BEGIN:VEVENT" in line:
+                course = []
+
+            elif "END:VEVENT" in line:
+                # Removes the lines that shouldn't be in list
+                tmp = self.__rmEntries(course)
+                data = Course()
+                data.setValues(tmp)
+                self.__classList.append( data )
+
                 pass
 
-    return classList
+            else:
+                try:
+                    # Fuck newlines
+                    course.append(line.strip("\n"))
+                except:
+                    pass
+
+
+
+if __name__ == "__main__":
+    with open("data.ics") as file:
+        extractor = Extractor({"UID","DTSTAMP","LOCATION"})
+        extractor.extractFromFile(file)
+        extractor.getClassList()[0].printValues()
+
+
