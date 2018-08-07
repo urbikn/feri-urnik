cca40aefc4d01f83835e6702425b353352a25ae5
Urban Knupleš <urban@knuples.net>
2018-08-07 15:37:57 +0200

    Refactor downloader.py
    - Changed structure of program by making it more PEP 8 (not entirely),
    - made script shorter and more readable to allow easier maintenance,
    - added comments to functions to understand their purpose to allow easier
      maintenance and further development,
    - moved code to two private functions 'downloadFile' and 'clickCourse'.

--- lib/utils/downloader.py
+++ lib/utils/downloader.py
@@ -9,6 +9,8 @@
 """
     Downloads the data of the URL: http://wise-tt.com/wtt_um_feri/
 
+    The entire download process is done by using Selenium and Firefox
+
 """
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -25,6 +27,7 @@ from pathlib import Path
 
 class Download:
     browser = None
+    url = None
     course = None
     filename = None
     renameFile = None
@@ -34,77 +37,108 @@ class Download:
 
 
 
-    def __init__(self, course, downloadPath):
+    def __init__(self, url, course, downloadPath):
         self.profile.set_preference("browser.download.folderList", 2)
         self.profile.set_preference("browser.download.manager.showWhenStarting", False)
         self.profile.set_preference("browser.download.dir", downloadPath)
-        self.profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/octet-stream, text/calendar,application/vnd.sus-calendar,text/x-vcalendar")
-        self.options.add_argument("--headless")  # Option to hide browser
+        self.profile.set_preference("browser.helperApps.neverAsk.saveToDisk",
+                                    "application/octet-stream, text/calendar,application/vnd.sus-calendar,text/x-vcalendar")
+ #       self.options.add_argument("--headless")  # Option to hide browser (Comment out to see browser)
+        self.browser = webdriver.Firefox(firefox_profile=self.profile,
+                                         firefox_options=self.options)
+        self.url = url
         self.course = course
         self.downloadPath = downloadPath
         self.filename = downloadPath + "/calendar.ics"
         self.renameFile = downloadPath + "/data.ics"
 
 
-
-
     def setUp(self):
-        print("""\n\t--------------\nSpeed of loading the site and downloading of data may differ\nbased on internet connection and/or how much their server is loaded.\n\t--------------""")
+        """
+            Makes a connection to the website.
+
+            If it can't get to the website, it checks the state of internet connection.
+            If the connection is OK, it assumes that the URL in self.url is wrong.
+        """
+
+        print("""
+                    --------------
+        Speed of loading the site and of downloading
+        the data may differ based on internet
+        connection and/or how much their server is loaded.
+                     --------------
+        """)
+
+        # Tries to connect to website
         try:
-            self.browser = webdriver.Firefox(firefox_profile=self.profile, firefox_options=self.options)
             self.browser.get(self.url)
+
         except WebDriverException:
             import urllib
-            print("\nError: Cannot connect to website. turn on your shitty internet or maybe they changed the URL.\nInvestigating this crap...\n")
-
+            print("""\nError: Cannot connect to website.\nIt's maybe your internet or maybe they changed the URL.\nChecking!""")
             try:
                 urllib.request.urlopen("http://google.com") # Google can't go down right
             except urllib.error.URLError:
-                print("Fix your shitty network connection!")
+                print("Tried to connect to google.com. It's your internet.")
             else:
-                raise WebDriverException() 
+                print("Looks like they changed the URL.")
 
             sys.exit()
  
 
     def stop(self):
+        """
+            Kills browser process.
+        """
         self.browser.quit()
 
-    def downloadUrnik(self, n=0):
-        if n == 3:
-            print( "Couldn't download file" )
-            return
-
+    
+    def __clickCourse(self):
+        """
+            Finds the course dropdown menu and click to generate the courses 
+            and clicks one of the courses to generate the table.
+        """
         wait = WebDriverWait(self.browser, 4)
-
-        # Waits for browser to load
-        wait.until(EC.presence_of_element_located((By.ID, "content")))
-
-        print("Starting click for course.")
-        numberId = self.browser.execute_script("return $('.noBorderBasicTable')[1].children[0].children[1].children[0].children[0].children[2].id.match(/\d+/)[0]")
-
-        element = wait.until(EC.presence_of_element_located((By.ID, "form:j_idt"+ numberId +"_label")))
+        
+        # Finds course dropdown menu
+        print("Starting click dropdown of courses.")
+        
+        # Not using jquery to execute the click instead use Selenium, so it can fully load
+        script ="return $('.noBorderBasicTable:eq(1) label:first').get()[0].id.match(/\d+/)[0]"
+        numberID = self.browser.execute_script(script)
+        element = wait.until(EC.presence_of_element_located((By.ID, "form:j_idt"+ numberID +"_label")))
         element.click()
-        print("Clicked course menu.")
 
-        print("\nStarting click for {}.".format(self.course))
-        elementType = wait.until(EC.presence_of_element_located((By.ID, "form:j_idt"+ numberId +"_6")))
-        elementType.click()
-        print("Clicked {}.".format(self.course))
-
-        print("\nStarting to download file")
-        # It just works, their dynamic website is shitty beyond belief
-
-        # Sometimes it's the first, sometimes it's the second... 
         time.sleep(1)
-        buttonDownloadId = self.browser.execute_script("""return $("span:contains('iCal-teden')").parent()[0].id""")
+        
+        # Finds the course 
+        print("Starting click for",self.course)
+        script = """return $(document.getElementById('form:j_idt86_items'))
+                    .find("li[data-label='"""+ self.course + """']")[0].id
+                """
+        courseID = self.browser.execute_script(script)
+        elementType = wait.until(EC.presence_of_element_located((By.ID, courseID )))
+        elementType.click()
+
+
+    def __downloadFile(self):
+        wait = WebDriverWait(self.browser, 4)
+        
+        print("Starting to download file")
+        time.sleep(1)
+        
+        script = """return $("span:contains('iCal-teden')").parent()["""
+        buttonDownloadId = self.browser.execute_script(script + "0].id")
         if buttonDownloadId == "":
-            buttonDownloadId = self.browser.execute_script("""return $("span:contains('iCal-teden')").parent()[1].id""")
+            buttonDownloadId = self.browser.execute_script(script + "1].id")
 
         elementType = wait.until(EC.element_to_be_clickable((By.ID, buttonDownloadId)))
         elementType.click()
-        
+ 
+        # A basic progress bar loop till the file has downloaded
         for i in range(1,10):
+            
+            # Checks if the file hasn't been downloaded in in the directory self.downloadPath
             if( not any("calendar" in file for file in os.listdir(self.downloadPath)) ):
                 time.sleep(0.4)
                 sys.stdout.write("\rDownloading {}".format("."*i))
@@ -113,21 +147,43 @@ class Download:
                 print("\nDownloaded file.")
                 break
 
+            
+            
+    def downloadUrnik(self, n=0):
+        if n == 3:
+            print( "Couldn't download file" )
+            return
+
+        wait = WebDriverWait(self.browser, 4)
+
+        # Waits for browser to load website
+        wait.until(EC.presence_of_element_located((By.ID, "content")))
+
+        self.__clickCourse()
+        self.__downloadFile()
+        
         # Checks if downloaded file exists
         if( not any("calendar" in file for file in os.listdir(self.downloadPath)) ):
                 print(os.listdir(self.downloadPath))
-                print( "\n...\nDownloaded file doesn't exist\nStarting new download\n-------------------------\n" )
+                print("""\n...
+                          Downloaded file doesn't exist
+                          Starting new download
+                          -------------------------
+                       """)
                 time.sleep(1)
                 n += 1
                 self.downloadUrnik(n)
                 return
-
+            
         os.rename(self.filename, self.renameFile)
 
 
 if __name__ == "__main__":
-    mainPath = os.path.abspath(os.path.join( os.getcwd(), "../../data"))
-    download = Download("RIT UN", mainPath)
+    mainPath = os.path.abspath(os.path.join( os.getcwd(), "../../data")) + '/'
+    download = Download(url = "http://wise-tt.com/wtt_um_feri/",
+                        course = "RAČUNALNIŠTVO IN INFORMACIJSKE TEHNOLOGIJE UN (BU20)",
+                        downloadPath = mainPath)
+
     download.setUp()
     download.downloadUrnik()
     download.stop()
