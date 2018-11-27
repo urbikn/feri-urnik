074788190ce528cca1a84cde8f3f87ce0ef54359
Urban Knuples <urban.knuples@student.um.si>
2018-11-27 19:53:11 +0100

    Add main script for changing configuration of scheduler

---
+++ lib/config/main.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2018 urbikn <urbikn@knuples>
+#
+# Distributed under terms of the MIT license.
+
+"""
+    The main script to change program/year/course in confg/user_data.json file
+"""
+
+def Change(data, type):
+    '''
+    Uses data to get list of available options and user decides which one to chose by inputing a number of the option
+
+    args:
+        data (List)                             - the list of options 
+        type (int) (0:program,1:year,2:course ) - to specify configuration to change
+
+    return:
+        None
+    '''
+
+def Change(type):
+    '''
+    Gets data from lib.downloader.downloader & lib.downloader.menu to get list of available options and user decides which one to chose by inputing a number of the option
+
+    args:
+        type (int) (0:program,1:year,2:course ) - to specify configuration to change
+
+    return:
+        None
+    '''
+    pass
