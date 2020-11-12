246816a1233f990b3637574b9aa1ee36f4fac009
Urban Knupleš <urbikn@gmail.com>
2020-11-12 15:15:13 +0100

    Write documentation in util.py

--- urnik/lib/util.py
+++ urnik/lib/util.py
@@ -6,6 +6,7 @@ from pathlib import Path
 
 from unidecode import unidecode
 from fuzzywuzzy import fuzz
+from icalevents.icalparser import Event
 from icalevents.icalevents import events
 import yaml
 
@@ -32,38 +33,63 @@ def set_geckodriver() -> None:
     tar.close()
 
 
-def filter_schedule(all_events, config_file: str):
-    event_descriptions = [event.description for event in all_events]
+def filter_schedule(all_events: [Event], config_path: str) -> [Event]:
+    """
+    Given a list of events the function filters out the subjects with the groups not matching the
+    accepted groups specified by the user saved in the configuration file.
 
+    Subjects not specified in the configuration file are not filtered out.
+
+    @param all_events: List of events
+    @param config_path: Path to the configuration file
+    @return: List of filtered events
+    """
     # Load user settings
-    with open(config_file, "r") as f:
+    with open(config_path, "r") as f:
         data = yaml.load(f, Loader=yaml.FullLoader)
-        user_groups = data['user']['groups']
+        user_groups = data['user'].get('groups', None)
+
+    if user_groups is None:
+        return all_events
 
     filtered_events = []
+    for event in all_events:
 
-    # Every description of a course is made out of these items
-    # [name, type, organizers, groups]
-    for i, description in enumerate(event_descriptions):
-        items = description.split(",")
+        # Every description of a course is made out of these items
+        # [name, type, organizers, groups]
+        subject = [desc.strip() for desc in event.description.split(',')][0]
+        groups = get_groups(event)
 
         # TODO: explain that this just check if the subjects are similar
-        # useful for grammer mistakes and such
-        matches = [subject['group'] for subject in user_groups if
-                   fuzz.partial_ratio(subject['name'].lower(), items[0].lower()) >= 90]
+        # useful for grammar mistakes and such
+        matches = [user_subject['group'] for user_subject in user_groups if
+                   fuzz.partial_ratio(user_subject['name'].lower(), subject.lower()) >= 90]
 
         if len(matches):
             # TODO: checks if the group is anywhere in the item and if so add
-            if any([fuzz.partial_ratio(group, item) >= 80 for item in items for group in matches]):
-                filtered_events.append(all_events[i])
+            if any([fuzz.partial_ratio(group, user_group) >= 80 for group in groups for user_group in matches]):
+                filtered_events.append(event)
         else:
-            filtered_events.append(all_events[i])
+            filtered_events.append(event)
 
     return filtered_events
 
 
-# TODO: make start and end automatically using a function for beginning and end of week
-def extract_schedule(file: str, start: datetime.datetime, end: datetime.datetime, use_filter=False):
+def extract_schedule(file: str, start: datetime.datetime, end: datetime.datetime, use_filter=False) -> [Event]:
+    """
+    Extract events from .ics file and return the events in a list
+
+    Function extracts events by a datetime interval specified with the start and end parameters.
+
+    By setting the use_filter parameter, the function filters the events by looking at the assigned groups saved in
+    the programs configuration file. The filter will only affect the events in which there is a group.
+
+    @param file: Path to .ics file
+    @param start: start date
+    @param end: end date
+    @param use_filter: Use group filtering specified in configuration file
+    @return: List of extracted events of type icalevents.icalparser.Event
+    """
     events_list = events(file=file, start=start, end=end)
 
     if use_filter:
@@ -73,7 +99,16 @@ def extract_schedule(file: str, start: datetime.datetime, end: datetime.datetime
     return events_list
 
 
-def get_organizer(event) -> []:
+def get_organizer(event: Event) -> [str]:
+    """
+    Return organizers of the event stored in the events description.
+
+    The function does this by using regex to match valid Person names in the description string and store
+    the matches into a list.
+
+    @param event: Event instance of icalevents.icalparser.Event
+    @return: List of event organizers
+    """
     # Using regex to get organizer names
     person_name = re.compile(r"[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*")
 
@@ -93,7 +128,13 @@ def get_organizer(event) -> []:
     return organizers
 
 
-def get_groups(event) -> []:
+def get_groups(event: Event) -> [str]:
+    """
+    Return groups of the event stored in the events description.
+
+    @param event: Event instance of icalevents.icalparser.Event
+    @return: List of event groups
+    """
     organizers = get_organizer(event)
 
     description = [desc.strip() for desc in event.description.split(',')]
@@ -102,16 +143,25 @@ def get_groups(event) -> []:
     return description[last_organizer_index + 1:]
 
 
-def create_cell(data: str, lenght: int) -> str:
+def create_cell_line(data: str, length: int) -> str:
+    """
+    Function creates a string centered with content and padded on each side with ' ' (spaces), starting and
+
+    To specify the start and end of the cell the string has a prefix/suffix filled with character '#'.
+
+    @param data: Content of the line
+    @param length: Width of the cell
+    @return: String with transformed content
+    """
     string = "#"
 
     if data in ["#", " ", "&", "-", "=", "$"]:
-        for i in range(0, lenght):
+        for i in range(0, length):
             string += data
 
         string = string[:-1]
     else:
-        end = lenght - len(data) - 1
+        end = length - len(data) - 1
         for i in range(0, end):
             if i % 2 == 0:
                 data = "{} ".format(data)
@@ -123,23 +173,68 @@ def create_cell(data: str, lenght: int) -> str:
     return string
 
 
-def display_cell(event) -> []:
-    time = event.start.strftime("%H:%M") + " - " + event.end.strftime("%H:%M")
-    subject = event.summary + " ({})".format(event.description.split(',')[1].strip()) # saves "subject (type)"
+def create_cell(event: Event) -> [str]:
+    """
+    Turn an event into a table cell by extracting the events duration, subject, location, organizers and
+    subject groups (if any).
+
+    The function before constructing the cell calculates the longest string/line which determines the width
+    of the entire cell.
+
+    A picture of the functions returned cell joined the strings lists with '\n'.
+
+        #####################
+        #                   #
+        #    start - end    #
+        #   subject (type)  #
+        #     location      #
+        #    organizers     #
+        #      groups       #
+        #                   #
+        #####################
+
+    @param event: Event instance of icalevents.icalparser.Event
+    @return: List containing every line of the table cell
+    """
+    duration = event.start.strftime("%H:%M") + " - " + event.end.strftime("%H:%M")
+    subject = event.summary + " ({})".format(event.description.split(',')[1].strip())  # saves "subject (type)"
     location = event.location
     organizers = ", ".join(get_organizer(event))
     groups = ", ".join(get_groups(event))
 
-    table = ["#", " ", time, subject, location, organizers, groups, " ", "#"]
+    table = ["#", " ", duration, subject, location, organizers, groups, " ", "#"]
     largest_len = max([len(string) for string in table]) + 10
 
     for i in range(len(table)):
-        table[i] = create_cell(table[i], largest_len)
+        table[i] = create_cell_line(table[i], largest_len)
 
     return table
 
 
-def display_schedule(schedule) -> str:
+def display_schedule(schedule: [Event]) -> str:
+    """
+    Create a table from all the events in list.
+
+    The table generated from the events looks like the picture below, where events for each day are stored
+    in their own table. The cells for each event are concatenated to the end of the table, sorted by their
+    start time.
+
+        date: day
+        #####################
+        #                   #
+        #    start - end    #
+        #   subject (type)  #
+        #     location      #
+        #    organizers     #
+        #      groups       #
+        #                   #
+        #####################
+
+    The function returns a singe string containing all the tables separated by '\n'.
+
+    @param schedule: List of events
+    @return: Schedule in a string
+    """
     days = sorted(set([event.start.date() for event in schedule]))
     schedule_by_day = [[event for event in schedule if event.start.date() == day] for day in days]
 
@@ -152,7 +247,7 @@ def display_schedule(schedule) -> str:
 
         table = [""] * 9  # The height of the table
         for event in events_by_day:
-            cell = display_cell(event)
+            cell = create_cell(event)
             for i in range(len(table)):
                 table[i] += cell[i]
 
